3284f2862743675ab607897a079a08d0de1b9a2a
amoussa78 <moussa.aniss@gmail.com>
2016-07-19 17:01:56 +0200

    Create BoobleByMinutes

--- src/fitec/scala/booble/BoobleByMinutes.scala
+++ src/fitec/scala/booble/BoobleByMinutes.scala
@@ -3,16 +3,24 @@ package fitec.scala.booble
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
-class BoobleByMinutes {
+object BoobleByMinutes {
     def main(args: Array[String]): Unit = {
       
     val conf = new SparkConf().setAppName("Spark Pi").setMaster("local[2]")
     val spark = new SparkContext(conf)
     val inputFile = spark.textFile(args(0))
-    val count = inputFile.map(line => line.split(" ")(3).toDouble)
-                                       .reduce(_ + _);
-    println(count)
+    val hoursWords = inputFile.map(line => (line.split(" ")(1).split("_")(0), line.split(" ")(1).split("_")(1), line.split(" ")(1).split("--"), 1))
+                                                              .map(x => (find(x._1, x._2), x._3, x._4)).map { case (a,b,c) => (a,(b,c))}
+                              
     spark.stop()
   }
+    
+    def find(heures : String, minutes : String) : String ={
+      if (29 < Integer.parseInt(minutes)) {
+				"entre " + heures + "h30 et " + heures + "h59"
+			} else {
+				"entre " + heures + "h00 et " + heures + "h29"
+			}
+    }
 
 }
