1ebdb5cf5f08b5f23933f7f1f966996139264974
amoussa78 <moussa.aniss@gmail.com>
2016-07-21 10:32:44 +0200

    Create LastFMListening

--- src/fitec/spark/lastfm/LastFMListening.scala
+++ src/fitec/spark/lastfm/LastFMListening.scala
@@ -6,16 +6,19 @@ import org.apache.hadoop.mapred.lib.HashPartitioner
 
 import org.apache.hadoop.mapred.lib.MultipleOutputFormat
 
-object LastFMListener {
+
+import org.apache.spark.SparkContext._
+
+object LastFMListening {
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setAppName("Spark Pi").setMaster("local[2]")
     val spark = new SparkContext(conf)
     val inputFile = spark.textFile(args(0)) 
 
-    val input = inputFile.map(line => (line.split(" ")(1), line.split(" ")(2), line.split(" ")(3)))
-    val result = input.filter(x => isListening(x._2,x._3)).map(x =>(x._1, 1)).reduceByKey(_+_)
-    result.saveAsTextFile("/home/user13/recordTest/result")
+    val input = inputFile.map(line => (line.split(" ")(1), (Integer.parseInt(line.split(" ")(2)), Integer.parseInt(line.split(" ")(3)), Integer.parseInt(line.split(" ")(4)))))
+    val result = input.reduceByKey((a,b) => (a._1 + b._1 ,a._2 + b._2, a._3+ b._3)).map(a => (a._1, a._2._1 + a._2._2, a._2._3) ) //filter(isListening(, radio))
+    result.saveAsTextFile("/home/user13/recordTest/resultQ2")
 
     spark.stop()
   }
